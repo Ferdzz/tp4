bbbd6d7dbb8b33b1a9ec8df33ec8edcdbfd4810a
Klarius <Charles.binette@hotmail.fr>
2016-05-03 19:19:11 -0400

    Todo 1

---
+++ TP4/app/src/main/java/com/deguet/joris/tp4/TransactionDialog.java
@@ -0,0 +1,37 @@
+package com.deguet.joris.tp4;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by 1146390 on 2016-05-03.
+ */
+public class TransactionDialog extends DialogFragment {
+
+    /*
+    TODO btnvoirEtatCaisse
+    -- voir l'état du tirroir caisse en tout temps
+
+    TODO OnCreate
+    --afficher tous les possibilité d'argentphysique dans la liste
+
+
+    TODO btnlistener pour ajouter/soustraire
+    --ajouter des billet ou des pièces
+    --a 0 ne supprime pas de la liste mais empêche d'aller plus bas que 0
+
+
+
+    TODO btnlistener pour envoyer change
+     --creer un objet change et l'envoyer au monayeur pour completer transaction
+     --afficher un autre dialog avec l'argent a rendre total et les objet dans une liste
+
+    */
+}
