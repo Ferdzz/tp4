ce3d56f9c3306a392906375641b981829bfed355
Klarius <Charles.binette@hotmail.fr>
2016-05-16 17:00:16 -0400

    debut Test
    jajajajaja

--- TP4/app/src/androidTest/java/com/deguet/joris/tp4/TestPayer.java
+++ TP4/app/src/androidTest/java/com/deguet/joris/tp4/TestPayer.java
@@ -34,10 +34,10 @@ public class TestPayer implements TestInterface {
         product2p1.amount = 3;
 
         Assert.assertEquals(2.00F, Utils.calculateTotalPrice(listTest), 0.01F);
-
-
-
-
+        product2p1.amount = 4;
+        Assert.assertEquals(2.00F, Utils.calculateTotalPrice(listTest), 0.01F);
+        product2p1.amount = 5;
+        Assert.assertEquals(3.00F, Utils.calculateTotalPrice(listTest), 0.01F);
     }
     @Test
     public void TestProduitGratuit(){
@@ -46,6 +46,11 @@ public class TestPayer implements TestInterface {
     }
     @Test
     public void TestTaxes(){
+        List<Product> listTest = new ArrayList<Product>();
+        Product product1 = new Product("TEST1",1.00F,"725272730836",true,true);
+        Product product2 = new Product("TEST2",2.00F,"725272730836",true,false);
+        Product product3 = new Product("TEST3",3.00F,"725272730836",true,true);
+        Product product4 = new Product("TEST4",4.00F,"725272730836",true,true);
 
 
     }
